1acbd7fb84f290ac44acf90279e1cfa887301682
Christian G. Warden <cwarden@xerus.org>
2024-05-17 15:33:22 -0500

    Write Debug Output to stderr
    Signed-off-by: Christian G. Warden <cwarden@xerus.org>

--- pkg/util/debug.go
+++ pkg/util/debug.go
@@ -7,6 +7,6 @@ import (
 
 func Debug(format string, args ...interface{}) {
 	if os.Getenv("LOCKGATE_DEBUG") == "1" {
-		fmt.Printf("LOCKGATE_DEBUG: %s\n", fmt.Sprintf(format, args...))
+		fmt.Fprintf(os.Stderr, "LOCKGATE_DEBUG: %s\n", fmt.Sprintf(format, args...))
 	}
 }
